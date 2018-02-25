94444f09636825615e03ed99583fca6abd263974
Alberto Vargas <a.vargasmarte@gmail.com>
2018-02-24 22:20:22 -0500

    revamped with object constructors and materialize library

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,216 +1,128 @@
-
-//PSEUDOCODE
-
-/*Here's how the app works:
-
-   * There will be four crystals displayed as buttons on the page.
-
-   These buttons will be represented by buttons in my HTML document.
-
-   These will have their own ID and their and class that will link to
-   bootstrap buttons.
-    I'm thinking of looking for something that looks like a crystal from the
-    CSS bootstrap website if I can find it
-
-   These buttons will all be in the same div
-
-   In the javascript, I have to run a function that does a set of steps:
-    for each of the buttons, I want a random number to be generated and 
-    assigned to each button.
-
-    After each random number is generated and stored, I want to make sure 
-    these can be used as integers, and added together.
-
-    After this, I want to add this value to an HTML element in a row after
-    the row with all the crystals.
-
-    When this number is added, I want to make sure it's compared to a random
-    number generated at the beginning of the game, that the user gets.
-
-
-   * The player will be shown a random number at the start of the game.
-
-   This will be a simple Math.random() function that will be stored in a 
-   scope higher than the function calling the random number for the crystals
-
-   This number will be between 19-120
-
-
-   * When the player clicks on a crystal, it will add a specific amount of 
-   points to the player's total score. 
-
-     * Your game will hide this amount until the player clicks a crystal.
-     * When they do click one, update the player's score counter.
-
-      I will do this to see if it works but I would personally not update the
-      score counter until getting the final result, which is when the user 
-      knows if that returned value makes them winners or losers
-
-      what I mean is that for the user it makes no difference if their score 
-      is updated (it doesn't help me as a user to know the scores until these
-      are added)
-
-   * The player wins if their total score matches the random number from the
-    beginning of the game.
-
-      I will have an if statement that tests whether the sum of the random
-      numbers (one per button) equals the random chosen number at the 
-      beginning of the game
-        If so, then I will assign a value to a html element and insert that
-        content to the div that contains it (this will be the function that 
-        keeps the score as the user plays and wins/loses)
-        If not, then the same will happen but for losses
-
-   * The player loses if their score goes above the random number.
-      The steps are expressed above.
-      However, this is a bit confusing since I have to press all those buttons
-      to get the addition, so I would think that the player would lose if the 
-      numbers do not match regardless of whether the sum of randoms is above 
-      or below the random chosen number at the beginning. 
-
-
-   * The game restarts whenever the player wins or loses.
-      pretty self-explanatory
-
-     * When the game begins again, the player should see a new random number. 
-     Also, all the crystals will have four new hidden values. Of course, the 
-     user's score (and score counter) will reset to zero.
-
-        I have to be good about how I nest functions and create scopes through
-        this entire process
-
-   * The app should show the number of games the player wins and loses. 
-   To that end, do not refresh the page as a means to restart the game.*/
-      
 //code starts here
 
 //CRUCIAL STEP: ENGLOBAR DOCUMENTO EN DOCUMENT.READY(FUNCTION() {})
-$(document).ready(function() {
+// $(document).ready(function () {
 
-  // make some variables global to the runtime of the application
-  let gameSum; 
-  let compNumber;
-  let userNumber;
-  let wins;
-  let losses;
+// make some variables global to the runtime of the application
+let gameSum = 0;
+let wins = 1;
+let losses = 1;
+let randomNumber;
 
-  //declare an array that contains each crystal
-  const crystalList = [
-    $("#firstCrystal"),
-    $("#secondCrystal"),
-    $("#thirdCrystal"),
-    $("#fourthCrystal"),
-  ];
+// create an object called crystals
 
-    wins = 0;
-    losses = 0;
-  // we use a function to initialize crystalCollectors to guarantee that the app will reset
+// crystals have the following properties and methods:
+// an empty array where random numbers will be pushed into
+// an array of crystals numbered 1-4
+// an empty array where crystalButtons will be pushed to
+// a function that iterates over each crystal, uses jQuery to dynamically create materialize buttons, 
+// pushes random numbers between 1 and 12 to randomArray, and appends each button to a container in the browser
+// a function that generates a random number between 19 and 120, and uses jQuery to present it to the user
 
-  function initializeCrystalCollectors() {
-    gameSum = 0;
-    compNumber = "";
-    userNumber = [];
-    
-    // let's first empty some divs with the jQuery .empty() method
-    $("#compNumber, #sum").empty();
+const Crystal = function (min, max) {
+  // declare min and max numbers
+  this.min = 1;
+  this.max = 12;
+  // a property randomNumber to which a random number between min and max is assigned
+  this.randomNumber = Math.floor(Math.random() * (this.max - this.min) + this.min);
+  // a property that holds a materialize floating button
+  this.buttonIcon = $("<i>").attr("class", "material-icons").text("add");
+  this.button = $("<a>").attr(
+    {
+      class: "btn-floating btn-large waves-effect waves-light red game-button",
+      "data-number": this.randomNumber
+    }).append(this.buttonIcon);
+}
 
-    // we start with a function that creates a random number between 19 and 120 for the computer
-    function randomizeComputer(min, max) {
-      // a random number is assigned to compNumber
-      compNumber = Math.floor(Math.random() * (max - min) + min);
-
-      //we select the div with a matching name
-      let computerNumber = $("#compNumber");
-
-      //we create a div and add compNumber as html content to it
-      let newGameDiv = $("<div>");
-      newGameDiv.html("<p>" + compNumber + "</p>");
-      // append this new div gameContainer
-      computerNumber.append(newGameDiv); 
-    }
-
-    // we invoke randomizeComputer
-    randomizeComputer(19, 120);
-
-    // we now follow with a function that creates a random number between 1-12 assigned to the crystals
-    
-    function randomizeCrystal() {
-      // for every item in crystalList...
-      for (let i = 0; i < crystalList.length; i++) {
-        // pass the following function
-        function randomNumberUser(min, max,randomNumber) {
-            // when this function runs, a randomNumber between 1-12 is generated
-            randomNumber = Math.floor(Math.random() * (max - min) + min);
-            // we push randomNumber into userNumber
-            userNumber.push(randomNumber);
-        }
-        // we invoke the above function for every item...
-        randomNumberUser(1,12);  
-        // we the value of each userNumber to their respective crystal.
-        crystalList[i].val(userNumber[i]);  
-        }
-      }
-    
-    // and we invoke randomizeCrystal!
-    randomizeCrystal();
-
+// create a new object constructor for Crystals
+crystalArray = [1, 2, 3, 4];
+const CrystalConstructor = function () {
+  // declare an empty randomArray
+  this.newCrystals = [];
+  this.crystals = crystalArray.map(crystal => this.newCrystals.push(new Crystal(crystal)));
+  // function that takes Crystal objects and displays them to the user
+  this.display = function () {
+    this.newCrystals.forEach(crystal => $("#game-div").append(crystal.button));
+  }
+  console.log(this.newCrystals);
+  // we write a method that assigns a random computerNumber and displays it to the user between 19 and 120
+  this.randomComputer = function (min, max) {
+    min = 19;
+    max = 120;
+    randomNumber = Math.floor(Math.random() * (max - min) + min);
+    // we attach this randomNumber to an html element and append to #game-div
+    let randomNumberDiv = $("<div>").append(randomNumber);
+    $("#game-div").append(randomNumberDiv);
   }
 
-  // let's show the player his/her score
-    // we select relevant divs where scores will live
-    let userWins = $("#wins");
-    let userLosses = $("#losses");
-    
-    // select the class to operate on both divs at once
-    let userScore = $(".score");
-    
-    // next we create an on-click function for when the user clicks on the crystals
-    
-    // when the crystals are clicked...
-    $(".crystal").on("click", function() {
+  this.clickEvents = function () {
+    $(".game-button").on("click", function () {
 
       // gameSum will equal itself plus the crystal's integer value
-      gameSum = gameSum + Number($(this).val());
-        
-      // gameSum is added to #sum with the .text method  
-      $("#sum").text(gameSum);
+      gameSum = gameSum + Number(this.dataset.number);
+      console.log(gameSum);
+
+      // gameSum is added to #sum with the.text method
+      $("#game-sum").html(gameSum);
 
       // after showing the gameSum to the player we check on the conditions to determine wins/losses
-      
       // if gameSum is greater than compNumber ...
-      if (gameSum > compNumber) { 
-        // we declare a function gameLost
-
-        function gameLost() {
-          // we declare a variable lost equal to lossess incremented by one
-          let lost = losses++;
-          // we then add content to the #losses div 
-          $("#losses").text("Losses: " + lost);
-        }
-
-        // we invoke this gameLost function!
+      if (gameSum > randomNumber) {
         gameLost();
-        // then we initializeCrystalCollectors (restart the game)
-        initializeCrystalCollectors();
+        gameSum = 0;
+        test = new CrystalConstructor();
+        // $("#game-div").empty();
+        test.display();
+        test.randomComputer();
+        test.clickEvents();
       }
-
       // but if gameSum equals compNumber ...
-      else if (gameSum === compNumber) {
-        // we initializeCrystalCollectors once again
-        initializeCrystalCollectors();
-        // we declare a function gameWon!
-        function gameWon() {
-          let won = wins++;
-          $("#wins").text("Wins: " + won);
-        }
-
-        // and we invoke it!
+      else if (gameSum === randomNumber) {
+        // the game is won
         gameWon();
-      }
 
-    });
-  initializeCrystalCollectors();
-});
+        gameSum = 0;
+        test = new CrystalConstructor();
+        test.display();
+        test.randomComputer();
+      }
+    })
+  };
+}
+
+let test = new CrystalConstructor();
+test.display();
+test.randomComputer();
+test.clickEvents();
+// console.log(test);
+
+// we declare a function gameLost
+function gameLost() {
+  // we declare a variable lost equal to lossess incremented by one
+  let lost = losses++;
+  // we then create a lossesDiv div
+  let lossesDiv = $("<div>");
+  // add text to let the user know their score
+  lossesDiv.text("Losses: " + lost);
+  // we then append the score to #user-score
+  $("#user-score").append(lossesDiv);
+  // and we empty the #game-sum and #game-div div
+  $("#game-sum").empty();
+  $("#game-div").empty();
+}
+
+// we declare a function gameWon!
+function gameWon() {
+  let won = wins++;
+
+  let winsDiv = $("<div>");
+  winsDiv.text("Wins: " + won);
+  $("#user-score").append(winsDiv);
+  // and we empty the #game-sum and #game-div div
+  $("#game-sum").empty();
+  $("#game-div").empty();
+}
+
 
 
 
